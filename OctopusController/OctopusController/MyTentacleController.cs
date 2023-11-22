94ee925b37c8d3efe677cff3b9e5c68751a6f088
Emma <emma.polanco@enti.cat>
2023-11-22 13:04:06 +0100

    Created branch to start Exercise 1

--- OctopusController/OctopusController/MyTentacleController.cs
+++ OctopusController/OctopusController/MyTentacleController.cs
@@ -12,8 +12,6 @@ namespace OctopusController
 
     
     internal class MyTentacleController
-
-    //MAINTAIN THIS CLASS AS INTERNAL
     {
 
         TentacleMode tentacleMode;
@@ -25,19 +23,17 @@ namespace OctopusController
         //Exercise 1.
         public Transform[] LoadTentacleJoints(Transform root, TentacleMode mode)
         {
-            //TODO: add here whatever is needed to find the bones forming the tentacle for all modes
-            //you may want to use a list, and then convert it to an array and save it into _bones
             tentacleMode = mode;
 
             switch (tentacleMode){
                 case TentacleMode.LEG:
-                    //TODO: in _endEffectorsphere you keep a reference to the base of the leg
+
                     break;
                 case TentacleMode.TAIL:
-                    //TODO: in _endEffectorsphere you keep a reference to the red sphere 
+
                     break;
                 case TentacleMode.TENTACLE:
-                    //TODO: in _endEffectorphere you  keep a reference to the sphere with a collider attached to the endEffector
+
                     break;
             }
             return Bones;
